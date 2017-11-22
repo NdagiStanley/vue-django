4b1015377ed239c9eb85b482b6f76d9b3fb7576e
Stanley Ndagi <ndagis@gmail.com>
2017-11-22 10:49:25 +0300

    Upgrade to 2.0 plus additional edits

---
+++ meta.js
@@ -0,0 +1,129 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "version": {
+      "type": "string",
+      "required": true,
+      "label": "Project version",
+      "default": "0.1.0"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Django - Vue.js project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+    "private": {
+      "type": "boolean",
+      "default": true
+    },
+    "build": {
+      "type": "list",
+      "message": "Vue build",
+      "choices": [
+        {
+          "name": "Runtime + Compiler: recommended for most users",
+          "value": "standalone",
+          "short": "standalone"
+        },
+        {
+          "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
+          "value": "runtime",
+          "short": "runtime"
+        }
+      ]
+    },
+    "router": {
+      "type": "confirm",
+      "message": "Install vue-router?"
+    },
+    "lint": {
+      "type": "confirm",
+      "message": "Use ESLint to lint your code?"
+    },
+    "lintConfig": {
+      "when": "lint",
+      "type": "list",
+      "message": "Pick an ESLint preset",
+      "choices": [
+        {
+          "name": "Standard (https://github.com/standard/standard)",
+          "value": "standard",
+          "short": "Standard"
+        },
+        {
+          "name": "Airbnb (https://github.com/airbnb/javascript)",
+          "value": "airbnb",
+          "short": "Airbnb"
+        },
+        {
+          "name": "none (configure it yourself)",
+          "value": "none",
+          "short": "none"
+        }
+      ]
+    },
+    "unit": {
+      "type": "confirm",
+      "message": "Setup unit tests"
+    },
+    "runner": {
+      "when": "unit",
+      "type": "list",
+      "message": "Pick a test runner",
+      "choices": [
+        {
+          "name": "Jest",
+          "value": "jest",
+          "short": "jest"
+        },
+        {
+          "name": "Karma and Mocha",
+          "value": "karma",
+          "short": "karma"
+        },
+        {
+          "name": "none (configure it yourself)",
+          "value": "noTest",
+          "short": "noTest"
+        }
+      ]
+    },
+    "e2e": {
+      "type": "confirm",
+      "message": "Setup e2e tests with Nightwatch?"
+    }
+  },
+  "filters": {
+    ".eslintrc.js": "lint",
+    ".eslintignore": "lint",
+    "config/test.env.js": "unit || e2e",
+    "build/webpack.test.conf.js": "e2e || (unit && runner === 'karma')",
+    "test/unit/**/*": "unit",
+    "test/unit/index.js": "unit && runner === 'karma'",
+    "test/unit/jest.conf.js": "unit && runner === 'jest'",
+    "test/unit/karma.conf.js": "unit && runner === 'karma'",
+    "test/unit/specs/index.js": "unit && runner === 'karma'",
+    "test/unit/setup.js": "unit && runner === 'jest'",
+    "test/e2e/**/*": "e2e",
+    "src/router/**/*": "router"
+  },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nTo build and serve in Django:\n\n  ./deploy.sh\n\nDocumentation can be found at https://goo.gl/V4eN7h"
+};
